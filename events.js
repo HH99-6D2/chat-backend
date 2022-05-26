1cf382efc67cfd18892b4a1682d9fd896e731a63
junehan <junehan.dev@gmail.com>
2022-05-26 23:09:50 +0900

    FEAT: Join Leave Event

--- events.js
+++ events.js
@@ -1,30 +1,40 @@
 const formatMessage = require("./messages");
-const { userJoin, getCurrentUser } = require("./users");
+const {
+	userJoin,
+	getCurrentUser,
+	userLeave,
+	getRoomUsers,
+} = require("./users");
 
 module.exports = (io) => {
 	io.on("connection", (socket) => {
-		console.log(socket);
 		console.log("new WS conn", socket.id);
 		socket.on("joinRoom", ({ id, username, room }) => {
 			const user = userJoin(socket.id, id, username, room);
-			socket.emit("message", formatMessage("SYSTEM", "welcome to chat"));
+			socket.emit("message", formatMessage("_SYS", "welcome to chat"));
 			socket.join(user.room);
 			socket.broadcast
 				.to(user.room)
-				.emit("message", formatMessage("SYSTEM", "user joined"));
+				.emit(
+					"message",
+					formatMessage("_SYS", `user ${user.id} joined`)
+				);
 
 			socket.on("disconnect", () => {
-				io.emit(
-					"message",
-					formatMessage("SYSTEM", "user left the chat")
-				);
+				const user = userLeave(socket.id);
+				if (user) {
+					io.to(user.room).emit(
+						"message",
+						formatMessage("_SYS", `user ${user.id} left the chat`)
+					);
+				}
 			});
 
 			socket.on("chatMessage", (message) => {
 				const user = getCurrentUser(socket.id);
 				io.to(user.room).emit(
 					"message",
-					formatMessage(user.username, message)
+					formatMessage(user.id, message)
 				);
 			});
 		});
--- users.js
+++ users.js
@@ -1,8 +1,9 @@
 const users = [];
+const rooms = {};
 // JOIN USER TO Chat;
-//
 function userJoin(socketId, id, username, room) {
 	const user = { socketId, id, username, room };
+	rooms[`${room}`] = rooms[`${room}`] ? rooms[`${room}`] + 1 : 1;
 	users.push(user);
 	return user;
 }
@@ -11,7 +12,25 @@ function getCurrentUser(id) {
 	return users.find((user) => user.socketId === id);
 }
 
+function userLeave(id) {
+	const index = users.findIndex((user) => user.socketId === id);
+	let user = null;
+	if (index !== -1) {
+		user = users.splice(index, 1)[0];
+		rooms[`${user.room}`] !== 1
+			? rooms[`${user.room}`]--
+			: delete rooms[`${user.room}`];
+	}
+	return user;
+}
+
+function getRoomUsers(room) {
+	return users.filter((user) => user.room === room);
+}
+
 module.exports = {
 	userJoin,
 	getCurrentUser,
+	userLeave,
+	getRoomUsers,
 };
