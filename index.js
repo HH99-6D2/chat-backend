77e46ffd0c35a1f776aa846e2070aa080b67bc8b
junehan <junehan.dev@gmail.com>
2022-05-26 20:59:48 +0900

    SETUP: static files setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 3000 || process.env.PORT;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => console.log(`Sever run on ${PORT}`));
