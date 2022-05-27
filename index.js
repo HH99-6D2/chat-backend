e9c6e39e1561e1c6544d84c0c22fc55dacaeb2f6
junehan <junehan.dev@gmail.com>
2022-05-27 13:27:27 +0900

    CONF: cors setup

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const socketio = require("socket.io");
 
 const PORT = 3000 || process.env.PORT;
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 const server = http.createServer(app);
